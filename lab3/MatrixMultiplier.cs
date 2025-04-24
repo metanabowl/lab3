7101a007ea777787c4e7554ba943375a41503dab
student <student@DESKTOP-I3OGHQQ>
2025-04-24 16:07:06 +0200

    Dodaj pliki projektów.

---
+++ lab3/MatrixMultiplier.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    internal class MatrixMultiplier
+    {
+            public static double[,] GenerateMatrix(int size)
+            {
+                var rand = new Random();
+                var matrix = new double[size, size];
+                for (int i = 0; i < size; i++)
+                    for (int j = 0; j < size; j++)
+                        matrix[i, j] = rand.NextDouble() * 10;
+                return matrix;
+            }
+
+            public static void MultiplySequential(double[,] A, double[,] B, double[,] result, int size)
+            {
+                for (int i = 0; i < size; i++)
+                    for (int j = 0; j < size; j++)
+                    {
+                        double sum = 0;
+                        for (int k = 0; k < size; k++)
+                            sum += A[i, k] * B[k, j];
+                        result[i, j] = sum;
+                    }
+            }
+
+            public static void MultiplyParallel(double[,] A, double[,] B, double[,] result, int size, int maxThreads)
+            {
+                var options = new ParallelOptions() { MaxDegreeOfParallelism = maxThreads };
+                Parallel.For(0, size, options, i =>
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        double sum = 0;
+                        for (int k = 0; k < size; k++)
+                            sum += A[i, k] * B[k, j];
+                        result[i, j] = sum;
+                    }
+                });
+            }
+
+            public static bool CompareMatrices(double[,] m1, double[,] m2, double tolerance = 1e-6)
+            {
+                int size = m1.GetLength(0);
+                for (int i = 0; i < size; i++)
+                    for (int j = 0; j < size; j++)
+                        if (Math.Abs(m1[i, j] - m2[i, j]) > tolerance)
+                            return false;
+                return true;
+            }
+        }
+    }
---
+++ lab3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj rozmiar macierzy: ");
+            int size = int.Parse(Console.ReadLine());
+
+            Console.Write("Podaj ilość wątków: ");
+            int threads = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nMnożenie macierzy {size}x{size} - liczba wątków: {threads}");
+
+            double[,] A = MatrixMultiplier.GenerateMatrix(size);
+            double[,] B = MatrixMultiplier.GenerateMatrix(size);
+            double[,] resultSeq = new double[size, size];
+            double[,] resultPar = new double[size, size];
+
+            var sw = Stopwatch.StartNew();
+            MatrixMultiplier.MultiplySequential(A, B, resultSeq, size);
+            sw.Stop();
+            Console.WriteLine($"Czas (sekwencyjnie): {sw.ElapsedMilliseconds} ms");
+
+            sw.Restart();
+            MatrixMultiplier.MultiplyParallel(A, B, resultPar, size, threads);
+            sw.Stop();
+            Console.WriteLine($"Czas (równolegle): {sw.ElapsedMilliseconds} ms");
+
+            bool areEqual = MatrixMultiplier.CompareMatrices(resultSeq, resultPar);
+            Console.WriteLine($"Macierze wynikowe są identyczne? {(areEqual ? "Tak" : "Nie")}");
+        }
+    }
+}
