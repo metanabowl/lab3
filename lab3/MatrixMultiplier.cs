ed499a11ebd055398e819adcf6321aedcf992660
Kacper <kacper.karkosz03@gmail.com>
2025-05-08 12:19:16 +0200

    Dodano pobieranie parametrów od użytkownika

--- lab3/MatrixMultiplier.cs
+++ lab3/MatrixMultiplier.cs
@@ -1,88 +1,117 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading; 
+using System.Threading.Tasks; 
 
 namespace lab3
 {
+
     internal class MatrixMultiplier
     {
-            public static double[,] GenerateMatrix(int size)
-            {
-                var rand = new Random();
-                var matrix = new double[size, size];
-                for (int i = 0; i < size; i++)
-                    for (int j = 0; j < size; j++)
-                        matrix[i, j] = rand.NextDouble() * 10;
-                return matrix;
-            }
+        // Generuje kwadratową macierz o podanym rozmiarze wypełnioną losowymi wartościami double.
+        public static double[,] GenerateMatrix(int size)
+        {
+            var rand = new Random();
+            var matrix = new double[size, size];
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    matrix[i, j] = rand.NextDouble() * 10;
+            return matrix;
+        }
 
-            public static void MultiplySequential(double[,] A, double[,] B, double[,] result, int size)
-            {
-                for (int i = 0; i < size; i++)
-                    for (int j = 0; j < size; j++)
-                    {
-                        double sum = 0;
-                        for (int k = 0; k < size; k++)
-                            sum += A[i, k] * B[k, j];
-                        result[i, j] = sum;
-                    }
-            }
-
-            public static void MultiplyParallel(double[,] A, double[,] B, double[,] result, int size, int maxThreads)
-            {
-                var options = new ParallelOptions() { MaxDegreeOfParallelism = maxThreads };
-                Parallel.For(0, size, options, i =>
+        // Mnoży dwie macierze kwadratowe (A i B) sekwencyjnie i zapisuje wynik do macierzy 'result'.
+        public static void MultiplySequential(double[,] A, double[,] B, double[,] result, int size)
+        {
+            // Pętla po wierszach macierzy wynikowej
+            for (int i = 0; i < size; i++)
+                // Pętla po kolumnach macierzy wynikowej
+                for (int j = 0; j < size; j++)
                 {
-                    for (int j = 0; j < size; j++)
+                    double sum = 0;
+                    // Pętla po elementach do sumowania (kolumny A i wiersze B)
+                    for (int k = 0; k < size; k++)
+                        sum += A[i, k] * B[k, j]; // C[i, j] = sum(A[i, k] * B[k, j]) dla k od 0 do size-1
+                    result[i, j] = sum;
+                }
+        }
+
+        // Mnoży dwie macierze kwadratowe (A i B) równolegle przy użyciu Parallel.Fori zapisuje wynik do macierzy 'result'. Równoległość dotyczy zewnętrznej pętli (wierszy).
+        public static void MultiplyParallel(double[,] A, double[,] B, double[,] result, int size, int maxThreads)
+        {
+            // Ustawienie opcji równoległości z maksymalną liczbą wątków
+            var options = new ParallelOptions() { MaxDegreeOfParallelism = maxThreads };
+
+            // Parallel.For równolegla zewnętrzną pętlę (po wierszach 'i')
+            Parallel.For(0, size, options, i =>
+            {
+                // Wewnętrzne pętle (po kolumnach 'j' i elementach sumy 'k')
+                for (int j = 0; j < size; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < size; k++)
+                        sum += A[i, k] * B[k, j];
+                    result[i, j] = sum;
+                }
+            });
+        }
+
+        // Mnoży dwie macierze kwadratowe (A i B) równolegle przy użyciu manualnie tworzonych wątków i zapisuje wynik do macierzy 'result'. Dzieli wiersze macierzy wynikowej między wątki.
+        public static void MultiplyThreaded(double[,] A, double[,] B, double[,] result, int size, int threadCount)
+        {
+            // Tablica do przechowywania referencji do wątków
+            Thread[] threads = new Thread[threadCount];
+            // Obliczanie liczby wierszy do przetworzenia przez każdy wątek
+            int rowsPerThread = size / threadCount;
+
+            // Tworzenie i uruchamianie wątków
+            for (int t = 0; t < threadCount; t++)
+            {
+                // Określenie zakresu wierszy dla bieżącego wątku
+                int startRow = t * rowsPerThread;
+                // Ostatni wątek przetwarza pozostałe wiersze (obsługa przypadku, gdy size nie jest podzielne przez threadCount)
+                int endRow = (t == threadCount - 1) ? size : startRow + rowsPerThread;
+
+                // Tworzenie nowego wątku z lambdą zawierającą logikę mnożenia dla danego zakresu wierszy
+                threads[t] = new Thread(() =>
+                {
+                    // Pętla po wierszach przypisanych do tego wątku
+                    for (int i = startRow; i < endRow; i++)
                     {
-                        double sum = 0;
-                        for (int k = 0; k < size; k++)
-                            sum += A[i, k] * B[k, j];
-                        result[i, j] = sum;
+                        // Wewnętrzne pętle po kolumnach 'j' i elementach sumy 'k'
+                        for (int j = 0; j < size; j++)
+                        {
+                            double sum = 0;
+                            for (int k = 0; k < size; k++)
+                                sum += A[i, k] * B[k, j];
+                            result[i, j] = sum;
+                        }
                     }
                 });
+                threads[t].Start(); // Uruchomienie wątku
             }
 
-            public static bool CompareMatrices(double[,] m1, double[,] m2, double tolerance = 1e-6)
+            // Czekanie na zakończenie wszystkich wątków
+            foreach (Thread thread in threads)
+                thread.Join();
+        }
+
+        // Porównuje dwie macierzy kwadratowe, sprawdzając, czy ich elementy są prawie równe, z uwzględnieniem tolerancji dla błędów zmiennoprzecinkowych.
+        public static bool CompareMatrices(double[,] m1, double[,] m2, double tolerance = 1e-6)
+        {
+            // Sprawdzenie, czy macierze mają ten sam rozmiar
+            if (m1.GetLength(0) != m2.GetLength(0) || m1.GetLength(1) != m2.GetLength(1))
             {
-                int size = m1.GetLength(0);
-                for (int i = 0; i < size; i++)
-                    for (int j = 0; j < size; j++)
-                        if (Math.Abs(m1[i, j] - m2[i, j]) > tolerance)
-                            return false;
-                return true;
+                return false;
             }
-            public static void MultiplyThreaded(double[,] A, double[,] B, double[,] result, int size, int threadCount)
-            {
-                Thread[] threads = new Thread[threadCount];
-                int rowsPerThread = size / threadCount;
 
-                for (int t = 0; t < threadCount; t++)
-                {
-                    int startRow = t * rowsPerThread;
-                    int endRow = (t == threadCount - 1) ? size : startRow + rowsPerThread;
+            int size = m1.GetLength(0);
+            // Porównywanie elementów macierzy
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    // Sprawdzenie bezwzględnej wartości różnicy. Jeśli jest większa niż tolerancja, macierze są różne.
+                    if (Math.Abs(m1[i, j] - m2[i, j]) > tolerance)
+                        return false; // Znaleziono znaczącą różnicę
 
-                    threads[t] = new Thread(() =>
-                    {
-                        for (int i = startRow; i < endRow; i++)
-                        {
-                            for (int j = 0; j < size; j++)
-                            {
-                                double sum = 0;
-                                for (int k = 0; k < size; k++)
-                                    sum += A[i, k] * B[k, j];
-                                result[i, j] = sum;
-                            }
-                        }
-                    });
-                    threads[t].Start();
-                }
-
-                foreach (Thread thread in threads)
-                    thread.Join();
-            }
+            return true; // Wszystkie elementy są w granicach tolerancji
+        }
     }
-
 }
--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,13 +1,72 @@
 ﻿using System;
 using System.Diagnostics;
-using System.IO;
+using System.IO; 
+using System.Threading; 
 
 namespace lab3
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+
+            int matrixSize;
+            int numThreads;
+
+            // Pobieranie rozmiaru macierzy
+            while (true)
+            {
+                Console.Write("Podaj rozmiar macierzy: ");
+                string sizeInput = Console.ReadLine();
+                if (int.TryParse(sizeInput, out matrixSize) && matrixSize > 0)
+                {
+                    break; // Poprawne dane, wyjście z pętli
+                }
+                Console.WriteLine("Nieprawidłowy rozmiar. Podaj dodatnią liczbę całkowitą.");
+            }
+
+            // Pobieranie liczby wątków
+            while (true)
+            {
+                Console.Write("Podaj liczbę wątków: ");
+                string threadsInput = Console.ReadLine();
+                if (int.TryParse(threadsInput, out numThreads) && numThreads > 0)
+                {
+                    break; // Poprawne dane, wyjście z pętli
+                }
+                Console.WriteLine("Nieprawidłowa liczba wątków. Podaj dodatnią liczbę całkowitą.");
+            }
+
+            double[,] A = MatrixMultiplier.GenerateMatrix(matrixSize);
+            double[,] B = MatrixMultiplier.GenerateMatrix(matrixSize);
+
+            // Wykonanie mnożenia sekwencyjnego jako punktu odniesienia
+            double[,] resultSeq = new double[matrixSize, matrixSize];
+            var swSeq = Stopwatch.StartNew();
+            MatrixMultiplier.MultiplySequential(A, B, resultSeq, matrixSize);
+            swSeq.Stop();
+            Console.WriteLine($"Sekwencyjne zakończone. Czas: {swSeq.ElapsedMilliseconds} ms.");
+
+            // Wykonanie mnożenia równoległego (wysokopoziomowego - Parallel.For)
+            double[,] resultParHigh = new double[matrixSize, matrixSize];
+            var swHigh = Stopwatch.StartNew();
+            MatrixMultiplier.MultiplyParallel(A, B, resultParHigh, matrixSize, numThreads);
+            swHigh.Stop();
+            bool correctHigh = MatrixMultiplier.CompareMatrices(resultSeq, resultParHigh);
+            Console.WriteLine($"Parallel.For zakończone. Czas: {swHigh.ElapsedMilliseconds} ms, Poprawność: {correctHigh}.");
+
+            // Wykonanie mnożenia równoległego (niskopoziomowego - manualne wątki)
+            double[,] resultParLow = new double[matrixSize, matrixSize];
+            var swLow = Stopwatch.StartNew();
+            MatrixMultiplier.MultiplyThreaded(A, B, resultParLow, matrixSize, numThreads);
+            swLow.Stop();
+            bool correctLow = MatrixMultiplier.CompareMatrices(resultSeq, resultParLow);
+            Console.WriteLine($"Manualne wątki zakończone. Czas: {swLow.ElapsedMilliseconds} ms, Poprawność: {correctLow}.");
+
+
+            // Część do testów wydajności
+            /*
             int[] sizes = { 200, 500, 1000, 2000 };
             int[] threadsList = { 1, 2, 4, 8, 12, 24 };
             int repetitions = 3;
@@ -19,8 +78,11 @@ namespace lab3
 
                 foreach (int size in sizes)
                 {
+                    // Generowanie macierzy tylko raz dla danego rozmiaru
                     double[,] A = MatrixMultiplier.GenerateMatrix(size);
                     double[,] B = MatrixMultiplier.GenerateMatrix(size);
+
+                    // Wykonanie mnożenia sekwencyjnego raz dla danego rozmiaru, aby uzyskać wynik odniesienia
                     double[,] resultSeq = new double[size, size];
                     MatrixMultiplier.MultiplySequential(A, B, resultSeq, size);
 
@@ -31,36 +93,39 @@ namespace lab3
                         double avgTimeHigh = 0, avgTimeLow = 0;
                         bool correctHigh = true, correctLow = true;
 
+                        // Testowanie metody wysokopoziomowej (Parallel.For)
                         for (int i = 0; i < repetitions; i++)
                         {
-                            double[,] resultPar = new double[size, size];
+                            double[,] resultPar = new double[size, size]; // Tworzenie nowej macierzy wynikowej dla każdego przebiegu
                             var sw = Stopwatch.StartNew();
                             MatrixMultiplier.MultiplyParallel(A, B, resultPar, size, threads);
                             sw.Stop();
                             avgTimeHigh += sw.ElapsedMilliseconds;
-                            correctHigh &= MatrixMultiplier.CompareMatrices(resultSeq, resultPar);
+                            correctHigh &= MatrixMultiplier.CompareMatrices(resultSeq, resultPar); // Sprawdzanie poprawności wyniku
                         }
-                        avgTimeHigh /= repetitions;
-                        writer.WriteLine($"{size},Wysokopoziomowa,{threads},{avgTimeHigh},{correctHigh}");
+                        avgTimeHigh /= repetitions; // Obliczanie średniego czasu
+                        writer.WriteLine($"{size},Wysokopoziomowa,{threads},{avgTimeHigh},{correctHigh}"); // Zapis do pliku
 
+                        // Testowanie metody niskopoziomowej (manualne wątki)
                         for (int i = 0; i < repetitions; i++)
                         {
-                            double[,] resultLow = new double[size, size];
+                            double[,] resultLow = new double[size, size]; // Tworzenie nowej macierzy wynikowej dla każdego przebiegu
                             var sw = Stopwatch.StartNew();
                             MatrixMultiplier.MultiplyThreaded(A, B, resultLow, size, threads);
                             sw.Stop();
                             avgTimeLow += sw.ElapsedMilliseconds;
-                            correctLow &= MatrixMultiplier.CompareMatrices(resultSeq, resultLow);
+                            correctLow &= MatrixMultiplier.CompareMatrices(resultSeq, resultLow); // Sprawdzanie poprawności wyniku
                         }
-                        avgTimeLow /= repetitions;
-                        writer.WriteLine($"{size},Niskopoziomowa,{threads},{avgTimeLow},{correctLow}");
+                        avgTimeLow /= repetitions; // Obliczanie średniego czasu
+                        writer.WriteLine($"{size},Niskopoziomowa,{threads},{avgTimeLow},{correctLow}"); // Zapis do pliku
 
-                        writer.Flush(); // na wypadek awarii
+                        writer.Flush(); // Zapis bufora do pliku na wypadek awarii programu
                     }
                 }
             }
 
             Console.WriteLine("\nZakończono testy. Wyniki zapisano do pliku: wyniki.csv");
+        }*/
         }
     }
 }
